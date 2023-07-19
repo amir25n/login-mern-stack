68844d18ca7a767417e52e9ad1ec3c8671e2ecff
Yasin Mohammadzade <yasinmohammadzade85@gmail.com>
2023-07-19 00:48:59 -0700

    add submit handler method to disable default submit and add states to save the input values on inputs values change

--- client/src/Components/SignupForm/SignupForm.jsx
+++ client/src/Components/SignupForm/SignupForm.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./SignupForm.css"
 function SignupForm() {
+
+    const [userName , setUserName] = useState("")
+    const [userEmail , setUserEmail] = useState("")
+    const [userPassword , setUserpassword] = useState("")
+
+    const submitHandeling = (event) =>{
+        event.preventDefault()
+    }
+
+
   return (
     <div className="sinup-form-contianer">
         <h2>Signup Form</h2>
-        <form>
+        <form onSubmit={submitHandeling}>
             <input type="text" placeholder='User Name'  className='signup-input'/>
             <input type="email" placeholder='User Email' className='signup-input'/>
             <input type="password" placeholder='User Password' className='signup-input'/>

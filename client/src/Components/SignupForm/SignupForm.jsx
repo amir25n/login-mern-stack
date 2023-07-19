224d94dd29d4efebb4be05ab5fced452e6fdbf73
Yasin Mohammadzade <yasinmohammadzade85@gmail.com>
2023-07-19 00:51:21 -0700

    connect the input values to state values

--- client/src/Components/SignupForm/SignupForm.jsx
+++ client/src/Components/SignupForm/SignupForm.jsx
@@ -15,9 +15,9 @@ function SignupForm() {
     <div className="sinup-form-contianer">
         <h2>Signup Form</h2>
         <form onSubmit={submitHandeling}>
-            <input type="text" placeholder='User Name'  className='signup-input'/>
-            <input type="email" placeholder='User Email' className='signup-input'/>
-            <input type="password" placeholder='User Password' className='signup-input'/>
+            <input type="text" placeholder='User Name'  className='signup-input' value={userName} onChange={(e) => setUserName(e.target.value)}/>
+            <input type="email" placeholder='User Email' className='signup-input' value={userEmail} onChange={(e) => setUserEmail(e.target.value)}/>
+            <input type="password" placeholder='User Password' className='signup-input' value={userPassword} onChange={(e) => setUserpassword(e.target.value)}/>
             <input type='submit' className='submit-btn' value="signup" />
         </form>
     </div>

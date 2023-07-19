30857b27ea0189eb85998e633ca3cd5cd7a129bf
Yasin Mohammadzade <yasinmohammadzade85@gmail.com>
2023-07-19 00:45:09 -0700

    add styles to project

--- client/src/Components/SignupForm/SignupForm.jsx
+++ client/src/Components/SignupForm/SignupForm.jsx
@@ -3,11 +3,12 @@ import "./SignupForm.css"
 function SignupForm() {
   return (
     <div className="sinup-form-contianer">
+        <h2>Signup Form</h2>
         <form>
             <input type="text" placeholder='User Name'  className='signup-input'/>
             <input type="email" placeholder='User Email' className='signup-input'/>
             <input type="password" placeholder='User Password' className='signup-input'/>
-            <button className='submit-btn'>signup</button>
+            <input type='submit' className='submit-btn' value="signup" />
         </form>
     </div>
   )

9883570097897206913db0933ce8730381f61856
Yasin Mohammadzade <yasinmohammadzade85@gmail.com>
2023-07-19 00:18:09 -0700

    add signup form component jsx codes

--- client/src/Components/SignupForm/SignupForm.jsx
+++ client/src/Components/SignupForm/SignupForm.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
-
+import "./SignupForm.css"
 function SignupForm() {
   return (
-    <div>SignupForm</div>
+    <div className="sinup-form-contianer">
+        <form>
+            <input type="text" placeholder='User Name'  className='signup-input'/>
+            <input type="email" placeholder='User Email' className='signup-input'/>
+            <input type="password" placeholder='User Password' className='signup-input'/>
+            <button className='submit-btn'>signup</button>
+        </form>
+    </div>
   )
 }
 

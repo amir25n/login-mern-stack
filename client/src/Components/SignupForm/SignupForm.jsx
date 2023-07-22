64b8f7bac29a250126eb5cc131ce4f8aa2071d90
yasinmhmmdzd <yasinmohammadzade85@gmail.com>
2023-07-22 12:12:40 +0330

    status jsx and logic developed

--- client/src/Components/SignupForm/SignupForm.jsx
+++ client/src/Components/SignupForm/SignupForm.jsx
@@ -7,10 +7,12 @@ function SignupForm() {
     const [userEmail , setUserEmail] = useState("")
     const [userPassword , setUserpassword] = useState("")
     const [submitted , setIsSubmitted] = useState(false)
+    const [status , setStatus] = useState("")
 
     const submitHandeling = (event) =>{
         event.preventDefault()
         setIsSubmitted(true)
+        setStatus("pending")
         if(userName.length > 5  && userEmail.includes('@') && userEmail.includes('.') && userPassword.length > 6){
 
             axios.post("https://login-form.iran.liara.run/signup" , {
@@ -18,6 +20,7 @@ function SignupForm() {
                 userEmail,
                 userPassword
             })
+            .then(res => setStatus(res.data.message))
         }
     }
 
@@ -42,6 +45,15 @@ function SignupForm() {
                 <p className="err">user password should more than 6 charecters</p>
             )}
             <input type='submit' className='submit-btn' value="signup" />
+            {(submitted && status === "pending") && (
+                <p className="status">Pending...</p>
+            )}
+            {(submitted && status === "user-ok") && (
+                <p className="success">Signup SuccessFull ! redirecting...</p>
+            )}
+            {(submitted && status === "email-error") && (
+                <p className="err">Email is not valid</p>
+            )}
         </form>
     </div>
   )

c6560c283d5643884c02d425e7fb6a94341f09f8
amir25n <amir25nk@gmail.com>
2023-07-22 12:02:38 +0330

    fix(fetch data) : fix bug for fetch data

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 import dotenv from "dotenv";
 import express from "express";
+import cors from "cors";
 
 import { connectDB } from "./utils/db.js";
 import homeRoutes from "./routes/homeRoutes.js";
@@ -13,7 +14,16 @@ connectDB();
 const app = express();
 
 //* custome middleware
-app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
+app.use(
+    express.urlencoded({
+        limit: "10mb",
+        extended: true,
+        parameterLimit: 50000,
+    })
+);
+
+app.use(cors());
 
 //?routes
 app.use(homeRoutes);

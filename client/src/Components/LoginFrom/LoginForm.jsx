f3e4f19f98b9e7dde5d7850383a2a04ff691b364
yasinmhmmdzd <yasinmohammadzade85@gmail.com>
2023-07-23 11:41:44 +0330

    Add LoginFrom styles

--- client/src/Components/LoginFrom/LoginForm.jsx
+++ client/src/Components/LoginFrom/LoginForm.jsx
@@ -5,9 +5,9 @@ function LoginForm() {
     <div className='login-form-container'>
         <h2>Login Form</h2>
         <form method="post">
-            <input type="text" placeholder='userEmail'/>
-            <input type="text" placeholder='userPassword'/>
-            <input type="submit" value="Login"/>
+            <input type="text" placeholder='userEmail' className='login-input'/>
+            <input type="text" placeholder='userPassword' className='login-input'/>
+            <input type="submit" value="Login" className='submit-btn'/>
         </form>
     </div>
   )

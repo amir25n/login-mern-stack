42f49d81cb75b5de1d5cf5016dd817d0be47884c
amir25n <amir25nk@gmail.com>
2023-07-23 11:52:34 +0330

    feat(hash) : hashing password to save database

--- server/routes/homeRoutes.js
+++ server/routes/homeRoutes.js
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import bcrypt from "bcrypt";
 
 import { User } from "../model/User.js";
 
@@ -9,18 +10,26 @@ router.get("/", (req, res) => {
 });
 
 router.post("/signup", async (req, res) => {
-    console.log(req.body);
-    if (!req.body) {
-        return res.json({ message: "input is not valid" });
+    try {
+        if (!req.body) {
+            return res.json({ message: "input-error" });
+        }
+        const isMatch = await User.findOne({ userEmail: req.body.userEmail });
+        if (isMatch) {
+            return res.json({ message: "email-error" });
+        }
+        const { userEmail, userName, userPassword } = req.body;
+        const hash = await bcrypt.hash(userPassword, 10);
+        const user = await User.create({
+            userEmail,
+            userName,
+            userPassword: hash,
+        });
+        res.status(200).json({ message: "user-ok" });
+        res.end();
+    } catch (error) {
+        console.log(error);
     }
-    const isMatch = await User.findOne({ userEmail: req.body.userEmail });
-    if (isMatch) {
-        return res.json({ message: "email is already used !" });
-    }
-    const user = await User.create(req.body);
-    res.status(200);
-    console.log("user created !");
-    res.end();
 });
 
 export default router;

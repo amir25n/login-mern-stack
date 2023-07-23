d4e20344793ba2b9c587211f97526d64d8f22873
yasinmhmmdzd <yasinmohammadzade85@gmail.com>
2023-07-23 11:52:30 +0330

    set form submit handeling logic

--- client/src/Components/LoginFrom/LoginForm.jsx
+++ client/src/Components/LoginFrom/LoginForm.jsx
@@ -1,12 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./LoginForm.css"
 function LoginForm() {
+  const [userEmail , setUserEmail] = useState("")
+  const [userPassword , setUserpassword] = useState("")
+  const loginHandeling = (e) =>{
+    e.preventDefault()
+  }
   return (
     <div className='login-form-container'>
         <h2>Login Form</h2>
-        <form method="post">
-            <input type="text" placeholder='userEmail' className='login-input'/>
-            <input type="text" placeholder='userPassword' className='login-input'/>
+        <form method="post" onSubmit={loginHandeling}>
+            <input type="text" onChange={(e) => setUserEmail(e.target.value)} value={userEmail} placeholder='userEmail' className='login-input'/>
+            <input type="text" onChange={(e) => setUserpassword(e.target.value)} value={userPassword} placeholder='userPassword' className='login-input'/>
             <input type="submit" value="Login" className='submit-btn'/>
         </form>
     </div>

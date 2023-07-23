11cdc1a1f3cc23c0f6d909eac89e715f0470d371
yasinmhmmdzd <yasinmohammadzade85@gmail.com>
2023-07-23 12:53:54 +0330

    add validation and status messages JSX and styles

--- client/src/Components/LoginFrom/LoginForm.jsx
+++ client/src/Components/LoginFrom/LoginForm.jsx
@@ -3,16 +3,42 @@ import "./LoginForm.css"
 function LoginForm() {
   const [userEmail , setUserEmail] = useState("")
   const [userPassword , setUserpassword] = useState("")
+  const [submitted , setIsSubmitted] = useState(false)
+  const [status , setStatus] = useState("")
   const loginHandeling = (e) =>{
     e.preventDefault()
+    setIsSubmitted(true)
+    setStatus("pending")
+    if(userEmail.includes('@') && userEmail.includes('.') && userPassword.length > 6){
+      console.log("submited");
+      setStatus("success")
+    }
+    else{
+      setStatus("")
+    }
   }
   return (
     <div className='login-form-container'>
         <h2>Login Form</h2>
         <form method="post" onSubmit={loginHandeling}>
             <input type="text" onChange={(e) => setUserEmail(e.target.value)} value={userEmail} placeholder='userEmail' className='login-input'/>
+            {(submitted && !userEmail.includes('@')) && (
+                <p className="err">email should has @</p>
+            )}
+            {(submitted && !userEmail.includes('.')) && (
+                <p className="err">email should has .</p>
+            )}
             <input type="text" onChange={(e) => setUserpassword(e.target.value)} value={userPassword} placeholder='userPassword' className='login-input'/>
+            {(submitted && userPassword.length < 5) && (
+                <p className="err">user password should more than 6 charecters</p>
+            )}
             <input type="submit" value="Login" className='submit-btn'/>
+            {(submitted && status === "pending") && (
+                <p className="status">Pending...</p>
+            )}
+            {(submitted && status === "success") && (
+                <p className="success">Login SuccessFull ! redirecting...</p>
+            )}
         </form>
     </div>
   )
